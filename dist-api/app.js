2a23c4f125557b61088b2c79e3518394ca0887c8
ald0111 <114394233+ald0111@users.noreply.github.com>
2023-08-18 15:45:28 +0530

    Second

--- dist-api/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _server = _interopRequireDefault(require("./server.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var server = new _server["default"]();
-console.log("Listening on port 3333");
-server.listen(3333);
