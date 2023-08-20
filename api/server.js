7066eb8f25c41ebfb9b7f1aa474a3a1e98c5e34b
ald0111 <114394233+ald0111@users.noreply.github.com>
2023-08-20 21:44:09 +0530

    basic websocket connection done

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 class Server {
   constructor() {
     this.app = express();
@@ -7,14 +7,31 @@ class Server {
     this.wsS.on("connection", (socket) => {
       //code to execute on connection
     });
+    this.connected = 0;
   }
   listen(port) {
     const server = this.app.listen(port || 3000);
     server.on("upgrade", (request, socket, head) => {
       this.wsS.handleUpgrade(request, socket, head, (sockConn) => {
+        this.connected++;
+        console.log("connected: " + this.connected);
         sockConn.on("message", (message) => {
-          console.log(message);
-          sockConn.send("hello");
+          //print every incoming message as text to the console
+          try {
+            let msg = console.log(JSON.parse(message));
+            this.wsS.clients.forEach((client) => {
+              if (client !== sockConn && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(msg));
+              }
+            });
+          } catch (e) {
+            console.log(e);
+          }
+          //broadcast every message to all connected clients except the sender
+        });
+        sockConn.on("close", () => {
+          this.connected--;
+          console.log("connected: " + this.connected);
         });
         this.wsS.emit("connection", socket, request);
       });
