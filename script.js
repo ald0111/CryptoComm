7b325648efae2bc71f9175631abbe8bf0a1721fa
ald0111 <114394233+ald0111@users.noreply.github.com>
2023-08-24 20:12:10 +0530

    Chat apps works

--- script.js
+++ script.js
@@ -1,128 +1,164 @@
-const serverUrl = "ws://localhost:3333";
-let aesKey = null;
-// Check if the browser supports the Web Crypto API
-if (window.crypto && window.crypto.subtle) {
-  // Define the key configuration
-  const keyConfig = {
-    name: "AES-GCM", // Algorithm name (you can use other algorithms as well)
-    length: 256, // Key length in bits
-  };
+const serverUrl = "ws://" + window.location.hostname + ":3333";
 
-  // Generate the random key
-  window.crypto.subtle
-    .generateKey(keyConfig, true, ["encrypt", "decrypt"])
-    .then((cryptoKeyPair) => {
-      console.log("Random CryptoKey pair generated:", cryptoKeyPair);
-      aesKey = cryptoKeyPair;
-      // Now you have the CryptoKey object(s) in cryptoKeyPair
-    })
-    .catch((error) => {
-      console.error("Error generating CryptoKey:", error);
-    });
-} else {
-  console.error("Web Crypto API is not supported in this browser.");
+const chatBox = document.getElementById("chat-box");
+const hashBox = document.getElementById("hash-box");
+const chatInput = document.getElementById("chat-input");
+const sendButton = document.getElementById("send-button");
+const connectInput = document.getElementById("connect-input");
+const connectButton = document.getElementById("connect-button");
+const targetUserIdInput = document.getElementById("target-user-id");
+const requestKeyExchangeButton = document.getElementById(
+  "request-key-exchange"
+);
+const availableKeysList = document.getElementById("available-keys-list");
+const username = document.getElementById("username");
+
+const userUniqueId = prompt("Enter your unique ID:");
+const userKeys = {}; // Store other users' keys with their unique IDs
+username.innerText = userUniqueId.toUpperCase();
+
+const socket = new WebSocket(serverUrl);
+
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateString(length) {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
-document.addEventListener("DOMContentLoaded", () => {
-  let chatBox;
-  let chatInput;
-  let sendButton;
-  let connectInput;
-  let connectButton;
-  chatBox = document.getElementById("chat-box");
-  chatInput = document.getElementById("chat-input");
-  sendButton = document.getElementById("send-button");
-  connectInput = document.getElementById("connect-input");
-  connectButton = document.getElementById("connect-button");
+const myKey = generateString(50);
 
-  const userUniqueId = prompt("Enter your unique ID:");
-  const userKeys = {}; // Store other users' keys with their unique IDs
+socket.addEventListener("open", (event) => {
+  console.log("WebSocket connection established.");
+});
 
-  const socket = new WebSocket(serverUrl);
-
-  socket.addEventListener("open", (event) => {
-    console.log("WebSocket connection established.");
-    // socket.send(
-    //   JSON.stringify({ type: "key_exchange_request", userId: userUniqueId })
-    // );
-  });
-
-  socket.addEventListener("message", (event) => {
-    const data = JSON.parse(event.data);
-
-    if (data.type === "key_exchange_response" && data.userId !== userUniqueId) {
-      const consent = confirm(
+socket.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
+  console.log(data);
+  if (data.type === "key_exchange_request" && data.targetId == userUniqueId) {
+    console.log("hiiiiii");
+    let consent = false;
+    if (data.handshake == "ACK") {
+      if (data.targetId == userUniqueId) {
+        consent = true;
+      }
+    } else {
+      consent = confirm(
         `User ${data.userId} wants to exchange keys. Do you agree?`
       );
-      if (consent) {
-        userKeys[data.userId] = data.key;
-        console.log(`Keys exchanged with user ${data.userId}.`);
+    }
+    if (consent) {
+      userKeys[data.userId] = data.userKey;
+      console.log(`Keys exchanged with user ${data.userId}.`);
+      if (data.handshake == "SYN") {
+        socket.send(
+          JSON.stringify({
+            type: "key_exchange_request",
+            targetId: data.userId,
+            userId: userUniqueId,
+            userKey: myKey,
+            handshake: "ACK",
+          })
+        );
       }
-    } else if (data.type === "encrypted_message") {
-      const decryptedMessage = decryptMessage(
-        data.message,
-        userKeys[data.senderId]
-      );
-      displayMessage(`${data.senderId}: ${decryptedMessage}`);
+      updateKeyAvailability();
     }
-  });
+    //   } else if (data.type === "encrypted_message" && userKeys[data.senderId]) {
+  } else if (data.type === "encrypted_message" && userKeys[data.uId]) {
+    const decryptedMessage = decryptMessage(data.message, userKeys[data.uId]);
+    console.log(decryptedMessage);
+    displayMessage(`${data.uId}: ${decryptedMessage}`);
+    displayHashMessage(`${data.uId}: ${data.message}`);
 
-  sendButton.addEventListener("click", () => {
-    const message = chatInput.value;
-    if (message) {
-      const encryptedMessage = encryptMessage(message, userKeys);
-      socket.send(
-        JSON.stringify({ type: "encrypted_message", message: encryptedMessage })
-      );
-      chatInput.value = "";
-    }
-  });
-
-  function encryptMessage(message, keys) {
-    const encryptedMessages = {};
-    for (const userId in keys) {
-      const key = keys[userId];
-      const textEncoder = new TextEncoder();
-      const encodedMessage = textEncoder.encode(message);
-      const encryptedData = crypto.subtle.encrypt(
-        { name: "AES-GCM", iv: new Uint8Array(12) },
-        key,
-        encodedMessage
-      );
-      encryptedMessages[userId] = encryptedData;
-    }
-    return encryptedMessages;
+    // displayMessage(`${data.senderId}: ${decryptedMessage}`);
+  } else if (data.type === "encrypted_message") {
+    displayHashMessage(`${data.uId}: ${data.message}`);
   }
+});
 
-  function decryptMessage(encryptedData, key) {
-    const decryptedMessage = crypto.subtle.decrypt(
+sendButton.addEventListener("click", () => {
+  const message = chatInput.value;
+  if (message) {
+    // const encryptedMessage = encryptMessage(message, myKey);
+    const encryptedMessage = CryptoJS.AES.encrypt(message, myKey);
+    displayMessage(`${userUniqueId}: ${message}`);
+    displayHashMessage(`${userUniqueId}: ${encryptedMessage.toString()}`);
+
+    let mm = JSON.stringify({
+      type: "encrypted_message",
+      uId: userUniqueId,
+      message: encryptedMessage.toString(),
+    });
+    console.log(mm);
+    socket.send(mm);
+    chatInput.value = "";
+  }
+});
+
+requestKeyExchangeButton.addEventListener("click", () => {
+  const targetUserId = targetUserIdInput.value;
+  if (targetUserId && targetUserId !== userUniqueId) {
+    socket.send(
+      JSON.stringify({
+        type: "key_exchange_request",
+        targetId: targetUserId,
+        userId: userUniqueId,
+        userKey: myKey,
+        handshake: "SYN",
+      })
+    );
+  }
+  targetUserIdInput.value = "";
+});
+
+function encryptMessage(message, keys) {
+  const encryptedMessages = {};
+  for (const userId in keys) {
+    const key = keys[userId];
+    const textEncoder = new TextEncoder();
+    const encodedMessage = textEncoder.encode(message);
+    const encryptedData = crypto.subtle.encrypt(
       { name: "AES-GCM", iv: new Uint8Array(12) },
       key,
-      encryptedData
+      encodedMessage
     );
-    const textDecoder = new TextDecoder();
-    return textDecoder.decode(decryptedMessage);
+    encryptedMessages[userId] = encryptedData;
   }
+  return encryptedMessages;
+}
 
-  function displayMessage(message) {
-    const messageElement = document.createElement("p");
-    messageElement.textContent = message;
-    chatBox.appendChild(messageElement);
-    chatBox.scrollTop = chatBox.scrollHeight;
+function decryptMessage(encryptedData, key) {
+  const decryptedMessage = CryptoJS.AES.decrypt(encryptedData, key);
+  console.log(decryptedMessage.toString(CryptoJS.enc.Utf8));
+  return decryptedMessage.toString(CryptoJS.enc.Utf8);
+}
+
+function displayMessage(message) {
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  chatBox.appendChild(messageElement);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function displayHashMessage(message) {
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  hashBox.appendChild(messageElement);
+  hashBox.scrollTop = hashBox.scrollHeight;
+}
+
+function updateKeyAvailability() {
+  availableKeysList.innerHTML = "";
+  for (const userId in userKeys) {
+    const listItem = document.createElement("li");
+    listItem.textContent = userId.toUpperCase();
+    availableKeysList.appendChild(listItem);
   }
+}
 
-  // ... (your existing script code) ...
-
-  connectButton.addEventListener("click", () => {
-    const targetUserId = connectInput.value;
-    if (targetUserId && targetUserId !== userUniqueId) {
-      socket.send(
-        JSON.stringify({
-          type: "key_exchange_request",
-          userId: targetUserId,
-          key: aesKey,
-        })
-      );
-    }
-    connectInput.value = "";
-  });
-});
+updateKeyAvailability();
